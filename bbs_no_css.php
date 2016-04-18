a3693f2b9edbba9627cca6f62f1a5e7fc9649e56
huro3h_satoshi <huro3h@gmail.com>
2016-04-18 23:22:40 +0800

    DB登録まで

--- bbs_no_css.php
+++ bbs_no_css.php
@@ -1,42 +1,48 @@
 <?php
+	//POST送信が行われたら、下記の処理を実行
+	//テストコメント
+	if(isset($_POST) && !empty($_POST)){
+		$nickname = $_POST['nickname'];
+		$comment = $_POST['comment'];
 
-  //POST送信が行われたら、下記の処理を実行
-  //テストコメント
-  if(isset($_POST) && !empty($_POST)){
+		//データベースに接続
+		$dsn = 'mysql:dbname=oneline_bbs;host=localhost';
+		$user = 'root';
+		$password = '';
+		$dbh = new PDO($dsn,$user,$password);
+		$dbh->query('SET NAMES utf8');
 
-    //データベースに接続
-
-    //SQL文作成(INSERT文)
-
-    //INSERT文実行
-
-    //データベースから切断
-
-    
-  }
+		//SQL文作成(INSERT文)
+		$sql = 'INSERT INTO `posts`(`id`, `nickname`, `comment`, `created`) VALUES (null,"'.$nickname.'","'.$comment.'",now())';
+		//INSERT文実行
+		$stmt = $dbh->prepare($sql);
+		$stmt->execute();
+		//データベースから切断
+		$dbh = null;
+	}
 ?>
 
 <!DOCTYPE html>
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
-  <title>セブ掲示版</title>
+  <title>Oneline_bbs_セブ掲示版</title>
 
 </head>
-<body>
-    <form action="bbs.php" method="post">
-      <input type="text" name="nickname" placeholder="nickname" required>
-      <textarea type="text" name="comment" placeholder="comment" required></textarea>
-      <button type="submit" >つぶやく</button>
-    </form>
+	<body>
+		<form action="bbs_no_css.php" method="post">
+			<input type="text" name="nickname" placeholder="nickname" required>
+			<textarea type="text" name="comment" placeholder="comment" required></textarea>
+			<button type="submit" >つぶやく</button>
+	    </form>
 
-    <h2><a href="#">nickname Eriko</a> <span>2015-12-02 10:10:20</span></h2>
-    <p>つぶやきコメント</p>
+	    <h2><a href="#">nickname Eriko</a> <span>2015-12-02 10:10:10</span></h2>
+	    <p>つぶやきコメント2</p>
 
-    <h2><a href="#">nickname Eriko</a> <span>2015-12-02 10:10:10</span></h2>
-    <p>つぶやきコメント2</p>
-</body>
+	    <h2><a href="#">nickname Eriko</a> <span>2015-12-02 10:10:10</span></h2>
+	    <p>つぶやきコメント2</p>
+	</body>
 </html>
 
-
+<!-- SELECT * FROM `posts` WHERE id=1 -->
 
